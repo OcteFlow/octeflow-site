2dee72e48f1f37c6f17b84d1cb17f450e0be6885
OcteFlow <rvillasenor111@gmail.com>
2026-03-06 20:38:55 -0600

    Create negocio.js

---
+++ pages/negocio.js
@@ -0,0 +1,8 @@
+export default function PyME() {
+  return (
+    <div style={{padding:"80px", fontFamily:"Arial"}}>
+      <h1>Soluciones para PyMEs</h1>
+      <p>Aquí irá el catálogo de servicios para pequeñas y medianas empresas.</p>
+    </div>
+  );
+}
