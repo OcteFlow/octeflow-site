2e281d2fe22fca4f043c335eac3fd0e0eafbb989
OcteFlow <rvillasenor111@gmail.com>
2026-03-12 13:50:07 -0600

    Update negocio.js

--- pages/negocio.js
+++ pages/negocio.js
@@ -3,6 +3,7 @@ import HeaderPages from "../components/HeaderPages";
 import BannerPage from "../components/BannerPage";
 import Footer from "../components/Footer";
 import CatalogSection from "../components/CatalogSection";
+import ProcessSection from "../components/ProcessSection";
 import CTASection from "../components/CTASection";
 
 import { catalogData } from "../data/catalogData";
@@ -21,7 +22,7 @@ export default function Negocio() {
     <BannerPage
 title="Soluciones para tu Negocio"
 subtitle="Diseñadas para mejorar la conectividad, la protección y la experiencia de tus clientes."
-image="/images/negocio.jpg"
+image="/negocio.jpg"
   />
 
       <CatalogSection
@@ -29,6 +30,8 @@ image="/images/negocio.jpg"
         items={catalogData.negocio}
       />
 
+        <ProcessSection/>
+        
         <CTASection
         title="Mejora la conectividad de tu negocio"
         text="Podemos evaluar tu red actual y proponerte una solución profesional."
