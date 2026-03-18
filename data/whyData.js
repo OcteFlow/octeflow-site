ca2f200544b115dd34b8c332a3ead690aa8ac9c1
OcteFlow <rvillasenor111@gmail.com>
2026-03-18 13:09:59 -0600

    Update whyData.js

--- data/whyData.js
+++ data/whyData.js
@@ -2,25 +2,25 @@ export const whyData = [
 
   {
     icon: "⚡",
-    title: "Infraestructura profesional",
+    title: "Infraestructura Profesional",
     text: "Diseñamos redes utilizando tecnologías empresariales para garantizar estabilidad y rendimiento."
   },
 
   {
+    icon: "🧠",
+title: "Soluciones a la Medida",
+text: "Analizamos tus necesidades y diseñamos una infraestructura tecnológica adaptada a tu espacio."
+  },
+  
+  {
     icon: "📡",
-    title: "Conectividad confiable",
+    title: "Conectividad Confiable",
     text: "Eliminamos zonas muertas y problemas de red para asegurar cobertura WiFi completa."
   },
 
   {
-    icon: "🔒",
-    title: "Seguridad tecnológica",
-    text: "Implementamos redes segmentadas y sistemas de seguridad que protegen tu infraestructura."
-  },
-
-  {
     icon: "📈",
-    title: "Soluciones escalables",
+    title: "Soluciones Escalables",
     text: "Diseñamos redes preparadas para crecer junto con tu empresa, negocio u hogar."
   }
 
