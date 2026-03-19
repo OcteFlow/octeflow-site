7b4681df2966e2c4f3df750a37cf7324a87cb924
OcteFlow <rvillasenor111@gmail.com>
2026-03-18 20:10:22 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
 import '../styles/globals.css';
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    const elements = document.querySelectorAll(".fade-up");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+          }
+        });
+      },
+      { threshold: 0.15 }
+    );
+
+    elements.forEach((el) => observer.observe(el));
+
+    return () => {
+      elements.forEach((el) => observer.unobserve(el));
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
